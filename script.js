48e7c6560c135ec5d9486599be9b5c16dfdcaa24
Daniel Phan <dannyphan@comcast.net>
2021-02-25 23:30:40 -0800

    Added content to JS that displays user's final score

--- script.js
+++ script.js
@@ -24,7 +24,17 @@ var jsQuestions = [{
     Question: "A very useful function used for debugging in JavaScript is called ____",
     Choices: ["localStorage", "getAttribute", "console.log", "setItems"],
     Answer: "console.log"
-}]
+},
+{
+    Question: "Arrays in JavaScript can be used to store ____",
+    Choices: ["Numbers & Strings", "Other Arrays", "Booleans", "All the above"],
+    Answer: "All the above"
+},
+{
+    Question: "Commonly used data types DO NOT include ____",
+    Choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+    Answer: "Alerts"
+}];
 
 console.log(jsQuestions);
 
@@ -37,9 +47,9 @@ var currentQuestion = 0;
 function startGame() {
     timerCount = 60;
     // Setting an attribute class of hide that hides the whole 'section' once game starts
-    sectionEl.setAttribute("class", "hide")
+    sectionEl.setAttribute("class", "hide");
     // Removing the attribute class of hide to display questions after section is hidden
-    showQuestions.removeAttribute("class", "hide")
+    showQuestions.removeAttribute("class", "hide");
     startTimer();
     setScore();
     nextQuestion();
@@ -58,7 +68,7 @@ function startTimer() {
 }
 
 function setScore() {
-    scoreElement.textContent = "Score: " + score;
+    scoreElement.textContent = "Score: " + score + "/100";
 }
 
 function nextQuestion() {
@@ -66,29 +76,29 @@ function nextQuestion() {
     var allQuestions = jsQuestions[currentQuestion];
 
     // Targets the class title in HTML and displays the question from allQuestions
-    titleEl.textContent = allQuestions.Question
+    titleEl.textContent = allQuestions.Question;
 
     // Clears the string of choices when the next question appears
     // innerHTML is used here because it's clearing everything including the values, text inside button, and etc 
-    choicesEl.innerHTML = ""
+    choicesEl.innerHTML = "";
 
     // for loop that creates a button for each choice in allQuestions
     for(i = 0; i < allQuestions.Choices.length; i++) {
         var choiceBtn = document.createElement("button");
         // Sets an attribute of value to each of the choices
-        choiceBtn.setAttribute("value", allQuestions.Choices[i])
+        choiceBtn.setAttribute("value", allQuestions.Choices[i]);
         // Displays the choices inside of the element button created on line 74
-        choiceBtn.textContent = allQuestions.Choices[i]
+        choiceBtn.textContent = allQuestions.Choices[i];
         // When user clicks on a button, then execute function answerCorrect, which will analyze whether the choice is correct or false
-        choiceBtn.onclick = answerCorrect
+        choiceBtn.onclick = answerCorrect;
         // targets the class choices in HTML and adds all the buttons + content to that div 
-        choicesEl.appendChild(choiceBtn)
+        choicesEl.appendChild(choiceBtn);
     }
 }
 
 function answerCorrect () {
     // This will grab the value from whatever the user selects
-    console.log(this.value)
+    console.log(this.value);
 
     // This says that if the value the user selects, isn't equal to the 'Answer' then subtract 10 seconds
     if (this.value !== jsQuestions[currentQuestion].Answer) {
@@ -96,7 +106,7 @@ function answerCorrect () {
     // If the value the user selects IS equal to the 'Answer' then add 20 points and display the score
     } else if (this.value === jsQuestions[currentQuestion].Answer) {
         score += 20;
-        scoreElement.textContent = "Score: " + score
+        scoreElement.textContent = "Score: " + score + "/100";
     }
     // If currentQuestion is equal to the length of how many questions are in jsQuestions, then end the game
     // If it doesn't equal jsQuestions.length, then continue and execute nextQuestion
@@ -106,7 +116,6 @@ function answerCorrect () {
     } else {
         nextQuestion();
     }
-
 }
 
 function gameOver () {
@@ -115,9 +124,9 @@ function gameOver () {
     
     showQuestions.setAttribute("class", "hide");
     highScore.removeAttribute("class", "show");
-    var highScore = document.createElement("h3")
-    highScore.textContent = "You scored " + score
-  
+    var finalScore = document.createElement("h2");
+    finalScore.textContent = "You scored " + score + "/100";
+    highScore.append(finalScore);
 }
 
 function resetGame() {
